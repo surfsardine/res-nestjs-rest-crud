fe0fbbc6f6005f6e8bc3d648af6bd6dc263d97ae
surfsardine <surfsardine@gmail.com>
2020-05-21 22:59:00 +0300

    add CropModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CropModule } from './crop/crop.module';
 
 @Module({
-    imports: [],
+    imports: [CropModule],
     controllers: [],
     providers: [],
 })
